ea87e173a96526eb39b92ee2d4f7d7d348c3521b
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-02-08 16:55:35 -0500

    New: Patch quantity

--- src/main/java/com/compras/compras/routers/FacturasRouter.java
+++ src/main/java/com/compras/compras/routers/FacturasRouter.java
@@ -1,7 +1,10 @@
 package com.compras.compras.routers;
 
 import com.compras.compras.model.Producto;
+import com.compras.compras.usecases.FindByIdUseCase;
 import com.compras.compras.usecases.FindByNameUseCase;
+import com.compras.compras.usecases.UpdateProduct;
+import com.compras.compras.usecases.UpdateProductUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +13,8 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -21,7 +22,7 @@ public class FacturasRouter {
     @Autowired
     WebClient webClient;
     @Bean
-    public RouterFunction<ServerResponse> findByName(FindByNameUseCase findByNameUseCase) {
+    public RouterFunction<ServerResponse> findById(FindByNameUseCase findByNameUseCase) {
         return route(
                 GET("/seekByName/{name}"),
                 request -> ServerResponse.ok()
@@ -31,4 +32,28 @@ public class FacturasRouter {
                         ))
         );
     }
+    @Bean
+    public RouterFunction<ServerResponse> seekById(FindByIdUseCase findByIdUseCase) {
+        return route(
+                GET("/seekById/{id}"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(findByIdUseCase
+                                .findById(request.pathVariable("id")),Producto.class
+                        ))
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> updateInventary(UpdateProductUseCase updateProductUseCase) {
+        return route(PATCH("/updateInventary/{id}/{quantity}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse
+                        .accepted()
+                        .body(BodyInserters
+                                .fromPublisher(updateProductUseCase
+                                                .updateById(request.pathVariable("id"),
+                                                        Integer.valueOf(request.pathVariable("quantity"))),
+                                        Producto.class)));
+    }
+
 }
--- src/main/java/com/compras/compras/usecases/SeekById.java
+++ src/main/java/com/compras/compras/usecases/SeekById.java
@@ -3,7 +3,6 @@ package com.compras.compras.usecases;
 import com.compras.compras.model.Producto;
 import reactor.core.publisher.Mono;
 
-public interface ProductRepository {
-    public Mono<Producto[]> findByName(String name);
-
+public interface SeekById {
+    public Mono<Producto> findById(String id);
 }

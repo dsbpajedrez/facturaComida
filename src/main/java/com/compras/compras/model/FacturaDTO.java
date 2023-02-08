636471bcc6d461fca3dc5458a27f8ec4800dc714
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-02-08 18:12:32 -0500

    New: Local date time of bill and bill created

--- src/main/java/com/compras/compras/model/FacturaDTO.java
+++ src/main/java/com/compras/compras/model/FacturaDTO.java
@@ -3,6 +3,8 @@ package com.compras.compras.model;
 import org.springframework.data.annotation.Id;
 
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -10,7 +12,7 @@ import java.util.Objects;
 public class FacturaDTO {
     @Id
     private String id;
-    private Date date;
+    private LocalTime date;
     private String idType;
     @NotBlank
     private String clientId;
@@ -21,7 +23,7 @@ public class FacturaDTO {
 
     }
 
-    public FacturaDTO(String id, Date date, String idType, String clientId, String clientName, List products) {
+    public FacturaDTO(String id, LocalTime date, String idType, String clientId, String clientName, List products) {
         this.id = id;
         this.date = date;
         this.idType = idType;
@@ -38,12 +40,12 @@ public class FacturaDTO {
         this.id = id;
     }
 
-    public Date getDate() {
+    public LocalTime getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setDate(LocalTime date) {
+        this.date= LocalTime.now().plusHours(-5);
     }
 
     public String getIdType() {
--- src/main/java/com/compras/compras/routers/FacturasRouter.java
+++ src/main/java/com/compras/compras/routers/FacturasRouter.java
@@ -1,10 +1,8 @@
 package com.compras.compras.routers;
 
+import com.compras.compras.model.FacturaDTO;
 import com.compras.compras.model.Producto;
-import com.compras.compras.usecases.FindByIdUseCase;
-import com.compras.compras.usecases.FindByNameUseCase;
-import com.compras.compras.usecases.UpdateProduct;
-import com.compras.compras.usecases.UpdateProductUseCase;
+import com.compras.compras.usecases.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +11,30 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.resourceLookupFunction;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class FacturasRouter {
     @Autowired
     WebClient webClient;
+    @Bean
+    public RouterFunction<ServerResponse> create(CreateFacturaUseCase createFacturaUseCase){
+        Function<FacturaDTO, Mono<ServerResponse>> executor = facturaDTO ->  createFacturaUseCase.apply(facturaDTO)
+                .flatMap(result -> ServerResponse.ok()
+                        .contentType(MediaType.TEXT_PLAIN)
+                        .bodyValue(result));
+        return route(
+                POST("/createBill").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(FacturaDTO.class).flatMap(executor)
+        );
+
+    }
     @Bean
     public RouterFunction<ServerResponse> findById(FindByNameUseCase findByNameUseCase) {
         return route(

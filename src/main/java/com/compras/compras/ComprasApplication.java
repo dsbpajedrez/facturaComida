2fc59508ef1cc4f56cd139027e0e2d6dc2db7a6e
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-02-11 21:05:14 -0500

    New: swagger config

--- src/main/java/com/compras/compras/ComprasApplication.java
+++ src/main/java/com/compras/compras/ComprasApplication.java
@@ -2,11 +2,16 @@ package com.compras.compras;
 
 import com.compras.compras.model.Producto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 
 @SpringBootApplication
+@EnableWebFlux
+@OpenAPIDefinition(info = @Info(title = "Swagger Demo", version = "1.0", description = "Documentation APIs v1.0"))
 public class ComprasApplication {
 
 	public static void main(String[] args) {
